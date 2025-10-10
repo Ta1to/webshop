834acc79479fb218dd916211abb1246d01f34e77
Ta1to <simon.hertweck.sh@gmail.com>
2025-10-10 14:00:31 +0200

    WEBS-0001: add vue-router and HomeView component for navigation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="home">
+        <h1>Willkommen zu evaris</h1>
+        <p>
+        Dies ist die Startseite. Verwenden Sie die Navigation, um zu anderen Seiten zu gelangen.
+        </p>
+    </div>
+</template>
